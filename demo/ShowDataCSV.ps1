2c32f94f3242bce9f11c47bc0f6e59abb9aaedea
dfinke <finked@hotmail.com>
2018-05-13 21:35:51 -0400

    added

---
+++ demo/ShowDataCSV.ps1
@@ -0,0 +1,15 @@
+﻿cls
+
+ipmo ..\Tabulator.psm1 -Force
+
+
+$co = $(
+    New-ColumnOption Name -editor true
+    New-ColumnOption age -formatter progress
+    New-ColumnOption rating -formatter star
+    New-ColumnOption driver -formatter tickCross
+)
+
+$to = New-TableOption -layout fitColumns -clipboard
+
+Import-Csv .\data.csv | Out-TabulatorView $co $to
