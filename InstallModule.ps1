c3c4240434ebb1fbf469659502764afb24a70f27
dfinke <finked@hotmail.com>
2018-05-28 12:36:03 -0400

    fixed name

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,3 @@
-$target = 'C:\Program Files\WindowsPowerShell\Modules\OutTablulatorView'
+$target = 'C:\Program Files\WindowsPowerShell\Modules\OutTabulatorView'
 
 robocopy . $target /mir /xf .gitignore /xd .git
