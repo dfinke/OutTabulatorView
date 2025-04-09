5e1ee6ff44cd95cc9ed52523c8ba254950fc1c1c
dfinke <finked@hotmail.com>
2025-04-09 17:33:31 -0400

    Refactor InstallModule.ps1 to dynamically determine module path and improve file copy logic

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,11 @@
-$target = 'C:\Program Files\WindowsPowerShell\Modules\OutTabulatorView'
-
-robocopy . $target /mir /xf .gitignore /xd .git
+param ($fullPath)
+#$fullPath = 'C:\Program Files\WindowsPowerShell\Modules\ImportExcel'
+if (-not $fullPath) {
+    $fullpath = $env:PSModulePath -split ":(?!\\)|;|," |
+        Where-Object {$_ -notlike ([System.Environment]::GetFolderPath("UserProfile")+"*") -and $_ -notlike "$pshome*"} |
+            Select-Object -First 1
+            $fullPath = Join-Path $fullPath -ChildPath "OutTabulatorView"
+}
+Push-location $PSScriptRoot
+Robocopy . $fullPath /mir /XD .vscode images .git .github CI __tests__ data mdHelp spikes /XF README.md README.original.md appveyor.yml azure-pipelines.yml .gitattributes .gitignore filelist.txt install.ps1 InstallModule.ps1 PublishToGallery.ps1
+Pop-Location
